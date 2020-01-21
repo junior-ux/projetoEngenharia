d41efa22688b032bb2e1bbcfde102816a914ef6e
André <handreluis.369@gmail.com>
2020-01-20 23:44:52 -0300

    Parte de cupons ajustada

--- pageUp usuario/PaginaPagamento/processaCupom.php
+++ pageUp usuario/PaginaPagamento/processaCupom.php
@@ -16,6 +16,7 @@ $consulta_p = mysqli_query($conn, $sql_p);
 
 $sql_cupom = "SELECT * FROM `ta_cupom` WHERE `nome` LIKE '$codigo'";
 $consulta_cupom = mysqli_query($conn, $sql_cupom);
+$linhas=mysqli_num_rows($consulta_cupom);
 
 
 while ($exibirCupom = mysqli_fetch_array($consulta_cupom)) {
@@ -29,20 +30,22 @@ while ($exibirAtivi = mysqli_fetch_array($consulta_p)) {
 
 
 $val_des = (($valor * $desconto) / 100);
+$_SESSION['val_des'] = $val_des;
 echo "<br>";
-echo $val_des. "<br>";
+//echo $val_des. "<br>";
 
-if (!$consulta_cupom) {
-    echo "Nenhum cupom encontrado";
-}else {
-    echo $codigo."<br>";
-    echo $valor;
-    echo "<br>";
-    echo $desconto;
-    $preco_final = $valor - $desconto;
-    echo $preco_final;
+if ($linhas > 0) {
+    //echo $codigo."<br>";
+    //echo $valor;
+    //echo "<br>";
+    //echo $desconto;
+    $preco_final = ($valor - $desconto);
+    //echo $preco_final;
     $_SESSION['preco_final'] = $preco_final;
     $_SESSION['msg'] = "<div class='alert alert-success'>Cupom adicionado com sucesso!</div>";
+    header("Location: PaginaPag1.php");
+}else {
+    $_SESSION['msg'] = "<div class='alert alert-success'>Cupom não encontrado!</div>";
     header("Location: PaginaPag1.php");
 }
 
